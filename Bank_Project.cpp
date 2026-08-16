7790512f3bd5a333605402b0146b9411c5438d85
aymanhabbashprogramming <aymanhabbashprogramming@gmail.com>
2026-08-16 15:13:45 +0300

    Add ReadNumber function

--- Bank_Project.cpp
+++ Bank_Project.cpp
@@ -2,9 +2,20 @@ using namespace std;
 #include <string>
 #include <iostream>
 
+int ReadNumber() {
+    int Number = 0;
+    cout << "Enter Number\n";
+    cin >> Number;
+    return Number;
+}
+
 int main()
 {
     cout << "Hello, I am on branch dev";
+
+    cout << endl;
+    cout << ReadNumber();
+
 
     return 0;
 }
