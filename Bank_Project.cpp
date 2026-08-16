e73254016b1635aba800a19a1072ba8a923f9f1d
aymanhabbashprogramming <aymanhabbashprogramming@gmail.com>
2026-08-16 16:11:27 +0300

    Change message to DEV VERSION

--- Bank_Project.cpp
+++ Bank_Project.cpp
@@ -11,7 +11,7 @@ int ReadNumber() {
 
 int main()
 {
-    cout << "________ Read Number Function ________\n";
+    cout << "________ DEV VERSION ________\n";
     cout << endl;
     cout << ReadNumber();
     cout << endl;
